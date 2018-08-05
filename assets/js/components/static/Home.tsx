84f2531b452ad70f5c9f15ac7fbd4f98b9673fa1
Kamil Zieliński <kamil.zielinski@netguru.co>
2018-08-05 20:48:38 +0200

    added text on home page image

--- assets/js/components/static/Home.tsx
+++ assets/js/components/static/Home.tsx
@@ -8,12 +8,20 @@ export class Home extends React.Component {
     return (
       <div className='home'>
         <div className='promo-photo'>
+          <div className='promo-photo__text'>
+            <div className='promo-photo__text--top'>
+              <b>Real-time</b> management
+            </div>
+            <div className='promo-photo__text--bottom'>
+              for speedcubing competition
+            </div>
+          </div>
           <img className='promo-img' src='images/promo.png' />
         </div>
         <div className='cards'>
           <Col s={12} m={8} className='cards__left'>
             test
-              </Col>
+          </Col>
           <Col s={12} m={4} className='cards__right no-padding'>
             <Col s={6} m={12} className='cards__right--top no-padding'>
               <HomeSmallCard
