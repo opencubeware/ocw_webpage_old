83b9f3eb9e8004c8900d2972aff2f98a7ce53784
Kamil Zieliński <kamil.zielinski@netguru.co>
2018-08-05 12:47:08 +0200

    change static pages to be used inside grid

--- assets/js/components/Main.tsx
+++ assets/js/components/Main.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react'
+import { Col, Row } from 'react-materialize';
 import { Route, Switch } from 'react-router-dom'
 import { About } from './static/About';
 import { Competitors } from './static/Competitors';
@@ -8,18 +9,27 @@ import { Events } from './static/Events';
 import { Home } from './static/Home';
 import { News } from './static/News';
 import { Team } from './static/Team';
+import { TableGrid } from './universal/TableGrid';
 
 export const Main = () => (
-  <div>
-    <Switch>
-      <Route exact={true} path='/' component={Home}/>
-      <Route path='/news' component={News}/>
-      <Route path='/events' component={Events}/>
-      <Route path='/competitors' component={Competitors}/>
-      <Route path='/dev' component={Dev}/>
-      <Route path='/about' component={About}/>
-      <Route path='/team' component={Team}/>
-      <Route path='/contact' component={Contact}/>
-    </Switch>
-  </div>
+  <Row className="home">
+    <Col m={3} className='grid'>
+      <TableGrid times={5} />
+    </Col>
+    <Col s={12} m={6} className='grid-6'>
+      <Switch>
+        <Route exact={true} path='/' component={Home} />
+        <Route path='/news' component={News} />
+        <Route path='/events' component={Events} />
+        <Route path='/competitors' component={Competitors} />
+        <Route path='/dev' component={Dev} />
+        <Route path='/about' component={About} />
+        <Route path='/team' component={Team} />
+        <Route path='/contact' component={Contact} />
+      </Switch>
+    </Col>
+    <Col m={3} className='grid'>
+      <TableGrid times={5} />
+    </Col>
+  </Row>
 )
--- assets/js/components/static/Home.tsx
+++ assets/js/components/static/Home.tsx
@@ -7,42 +7,32 @@ export class Home extends React.Component {
   public render() {
     return (
       <div>
-        <Row className="home">
-          <Col m={3} className='grid'>
-            <TableGrid times={5} />
-          </Col>
-          <Col s={12} m={6} className='grid-6'>
-            <div className='promo-photo'>
-              <img className='promo-img' src='images/promo.png' />
-            </div>
-            <div className='cards'>
-              <Col s={12} m={8} className='cards__left'>
-                test
+        <div className='promo-photo'>
+          <img className='promo-img' src='images/promo.png' />
+        </div>
+        <div className='cards'>
+          <Col s={12} m={8} className='cards__left'>
+            test
               </Col>
-              <Col s={12} m={4} className='cards__right no-padding'>
-                <Col s={6} m={12} className='cards__right--top no-padding'>
-                  <HomeSmallCard
-                    color='light'
-                    icon_path='images/envelope_icon.png'
-                    title='test'
-                    body='test'
-                  />
-                </Col>
-                <Col s={6} m={12} className='cards__right--bottom no-padding'>
-                  <HomeSmallCard
-                    color='dark'
-                    icon_path='images/calendar_icon.png'
-                    title='test'
-                    body='test'
-                  />
-                </Col>
-              </Col>
-            </div>
+          <Col s={12} m={4} className='cards__right no-padding'>
+            <Col s={6} m={12} className='cards__right--top no-padding'>
+              <HomeSmallCard
+                color='light'
+                icon_path='images/envelope_icon.png'
+                title='test'
+                body='test'
+              />
+            </Col>
+            <Col s={6} m={12} className='cards__right--bottom no-padding'>
+              <HomeSmallCard
+                color='dark'
+                icon_path='images/calendar_icon.png'
+                title='test'
+                body='test'
+              />
+            </Col>
           </Col>
-          <Col m={3} className='grid'>
-            <TableGrid times={5} />
-          </Col>
-        </Row>
+        </div>
       </div>
     );
   }
