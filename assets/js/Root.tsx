1a28226ab1b874e56822bbe6e232503ada9137ff
Kamil Zieliński <kamil.zielinski@netguru.co>
2018-08-08 23:00:11 +0200

    navbar fixed

--- assets/js/Root.tsx
+++ assets/js/Root.tsx
@@ -5,10 +5,8 @@ export default class Root extends React.Component<{}, {}> {
   public render(): JSX.Element {
     return (
       <div>
-        ...
         <Navbar />
         {this.props.children}
-        ...
       </div>
     )
   }
--- assets/js/components/navbar/Navbar.tsx
+++ assets/js/components/navbar/Navbar.tsx
@@ -3,30 +3,24 @@ import { Link } from 'react-router-dom'
 
 const Navbar = () =>
   <div className="navbar">
-    <div className="grid"/>
-    <div className="grid-6">
-      <div className="navbar__wrapper">
-        <div className="ocw-logo">
-          <Link to="/">
-            <img className="ocw__logo--image" src="images/logo.png" />
-          </Link>
-        </div>
-        <div>
-          <ul className="navbar__items">
-            <li><Link className="navbar__item" to="/news">News</Link></li>
-            <div className="slash">/</div>
-            <li><Link className="navbar__item" to="/events">Events</Link></li>
-            <div className="slash">/</div>
-            <li><Link className="navbar__item" to="/about">About</Link></li>
-            <div className="slash">/</div>
-            <li><Link className="navbar__item" to="/team">Team</Link></li>
-            <div className="slash">/</div>
-            <li><Link className="navbar__item" to="/contact">Contact</Link></li>
-          </ul>
-        </div>
-      </div>
+    <div className="navbar__ocw-logo">
+      <Link to="/">
+        <img className="ocw__logo--image" src="images/logo.png" />
+      </Link>
     </div>
-    <div className="grid"/>
+    <div className="navbar__items">
+      <ul>
+        <li><Link className="navbar__item" to="/news">News</Link></li>
+        <div className="slash">/</div>
+        <li><Link className="navbar__item" to="/events">Events</Link></li>
+        <div className="slash">/</div>
+        <li><Link className="navbar__item" to="/about">About</Link></li>
+        <div className="slash">/</div>
+        <li><Link className="navbar__item" to="/team">Team</Link></li>
+        <div className="slash">/</div>
+        <li><Link className="navbar__item" to="/contact">Contact</Link></li>
+      </ul>
+    </div>
   </div>
 
 export default Navbar
