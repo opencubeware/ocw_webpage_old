44f9521750a2898c16d808e8ee57971574776671
Kamil Zieliński <paryzowy@gmail.com>
2018-11-17 18:45:22 +0100

    fix Event.name functions

--- lib/ocw_webpage_web/schemas/event.ex
+++ lib/ocw_webpage_web/schemas/event.ex
@@ -13,4 +13,12 @@ defmodule OcwWebpageWeb.Schemas.Event do
   def name(%Event{event_name: %EventName{name: _name}} = preloaded_event) do
     preloaded_event.event_name.name
   end
+
+  def name(%Event{event_name: %Ecto.Association.NotLoaded{}, event_name_id: nil}) do
+    "Event does not have name"
+  end
+
+  def name(%Event{event_name: %Ecto.Association.NotLoaded{}, event_name_id: _id}) do
+    "EventName not preloaded"
+  end
 end
