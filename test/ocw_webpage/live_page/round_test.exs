3fac9a64e7e71a86df5e06598572b0c118a65a9d
Kamil Zieliński <paryzowy@gmail.com>
2018-11-19 23:46:17 +0100

    fix test warning

--- test/ocw_webpage/live_page/round_test.exs
+++ test/ocw_webpage/live_page/round_test.exs
@@ -18,7 +18,7 @@ defmodule OcwWebpage.LivePage.RoundTest do
     end
 
     test "displays error message when round_name not preloaded" do
-      round_name = build(:round_name, name: "TestName") |> with_round()
+      build(:round_name, name: "TestName") |> with_round()
       [round] = Repo.all(Round)
 
       assert Round.name(round) == {:error, :round_name_not_preloaded}
