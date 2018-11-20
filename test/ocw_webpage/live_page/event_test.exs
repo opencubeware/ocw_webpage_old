42656bbc8db2ca7437cf841ce1329330b4617492
Kamil <paryzowy@gmail.com>
2018-11-20 01:20:27 +0100

    update tests

--- test/ocw_webpage/live_page/event_test.exs
+++ test/ocw_webpage/live_page/event_test.exs
@@ -2,24 +2,35 @@ defmodule OcwWebpage.LivePage.EventTest do
   use OcwWebpage.DataCase, async: true
 
   alias OcwWebpage.LivePage.Event
+  alias OcwWebpage.Constants.EventName
 
   describe "name/1" do
-    test "displays name of related event_name" do
-      event_name = build(:event_name, name: "TestName")
-      event = build(:event, event_name: event_name)
-      assert Event.name(event) == {:ok, "TestName"}
+    setup do
+      [event_name | _] = Repo.all(EventName)
+
+      %{
+        tournament: build(:tournament),
+        event_name: event_name
+      }
     end
 
-    test "displays error message when event_name is not definned" do
-      insert(:event, event_name: nil)
+    test "displays name of related event_name", %{tournament: tournament, event_name: event_name} do
+      event = build(:event, event_name: event_name, tournament: tournament)
+      assert Event.name(event) == {:ok, "3x3x3"}
+    end
+
+    test "displays error message when event_name is not definned", %{tournament: tournament} do
+      insert(:event, event_name: nil, tournament: tournament)
       [event] = Repo.all(Event)
 
       assert Event.name(event) == {:error, :event_does_not_have_name}
     end
 
-    test "displays error message when event_name not preloaded" do
-      event_name = build(:event_name, name: "TestName")
-      insert(:event, event_name: event_name)
+    test "displays error message when event_name not preloaded", %{
+      tournament: tournament,
+      event_name: event_name
+    } do
+      insert(:event, event_name: event_name, tournament: tournament)
       [event] = Repo.all(Event)
 
       assert Event.name(event) == {:error, :event_name_not_preloaded}
--- test/ocw_webpage/live_page/round_test.exs
+++ test/ocw_webpage/live_page/round_test.exs
@@ -2,23 +2,35 @@ defmodule OcwWebpage.LivePage.RoundTest do
   use OcwWebpage.DataCase, async: true
 
   alias OcwWebpage.LivePage.Round
+  alias OcwWebpage.Constants.{EventName, RoundName}
 
   describe "name/1" do
-    test "displays name of related round_name" do
-      round_name = build(:round_name, name: "TestName")
-      round = build(:round, round_name: round_name)
-      assert Round.name(round) == {:ok, "TestName"}
+    setup do
+      tournament = build(:tournament)
+      [event_name | _] = Repo.all(EventName)
+      [round_name | _] = Repo.all(RoundName)
+      event = insert(:event, tournament: tournament, event_name: event_name)
+
+      %{event: event, round_name: round_name}
     end
 
-    test "displays error message when round_name is not definned" do
-      insert(:round)
+    test "displays name of related round_name", %{event: event, round_name: round_name} do
+      round = build(:round, event: event, round_name: round_name)
+      assert Round.name(round) == {:ok, "First Round"}
+    end
+
+    test "displays error message when round_name is not definned", %{event: event} do
+      insert(:round, event: event)
       [round] = Repo.all(Round)
 
       assert Round.name(round) == {:error, :round_does_not_have_name}
     end
 
-    test "displays error message when round_name not preloaded" do
-      build(:round_name, name: "TestName") |> with_round()
+    test "displays error message when round_name not preloaded", %{
+      event: event,
+      round_name: round_name
+    } do
+      insert(:round, event: event, round_name: round_name)
       [round] = Repo.all(Round)
 
       assert Round.name(round) == {:error, :round_name_not_preloaded}
