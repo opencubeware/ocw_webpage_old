aac2a5b3e0ea193c3562d9fd2bbb059682f6a7e6
Kamil <paryzowy@gmail.com>
2018-11-30 22:42:04 +0100

    bump verions

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule OcwWebpage.Mixfile do
     [
       app: :ocw_webpage,
       version: "0.0.1",
-      elixir: "~> 1.7",
+      elixir: "~> 1.7.1",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
@@ -35,6 +35,7 @@ defmodule OcwWebpage.Mixfile do
     [
       {:phoenix, "~> 1.3.3"},
       {:phoenix_pubsub, "~> 1.0"},
+      {:plug_cowboy, "~> 1.0"},
       {:ecto_sql, "~> 3.0"},
       {:postgrex, ">= 0.0.0"},
       {:mariaex, "~> 0.9.1"},
@@ -42,6 +43,7 @@ defmodule OcwWebpage.Mixfile do
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
       {:ex_machina, "~> 2.2"},
+      {:dialyxir, "~> 0.5.1", only: [:dev], runtime: false},
       {:cowboy, "~> 1.0"}
     ]
   end
