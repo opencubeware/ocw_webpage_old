3bca37e155faea97b3a9fbe9130a5aeff2552ada
Kamil <paryzowy@gmail.com>
2018-12-02 00:24:14 +0100

    fix schemas

--- lib/ocw_webpage/data_access/schemas/person.ex
+++ lib/ocw_webpage/data_access/schemas/person.ex
@@ -1,13 +1,13 @@
 defmodule OcwWebpage.DataAccess.Schemas.Person do
   use Ecto.Schema
 
-  alias OcwWebpage.DataAccess.Schemas.Result
+  alias OcwWebpage.DataAccess.Schemas.{Country, Result}
 
   schema "persons" do
     field(:first_name, :string)
     field(:last_name, :string)
     field(:wca_id, :string)
-    field(:country, :string)
+    belongs_to(:country, Country)
     has_many(:results, Result, foreign_key: :competitor_id)
   end
 end
